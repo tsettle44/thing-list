35dfeb7e671b729944a87fae457a36ea4c382c8a
Your Full Name <youremailaddress@example.com>
2017-05-24 11:51:47 -0400

    add firebase

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 import Header from './Header'
 import ThingList from './ThingList'
 import AddThingButton from './AddThingButton'
+import base from './base'
 
 class App extends Component {
+
+  componentWillMount() {
+   this.ref = base.syncState(
+      'things', 
+      {
+        context: this,
+        state: 'things'
+      }
+    )
+  }
+
   state = {
     things: {}
   }
