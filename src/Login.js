a66760bbe849adb4641704c6c33297dc12f55f51
Your Full Name <youremailaddress@example.com>
2017-05-25 10:01:50 -0400

    add sign in button

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Login.css'
+
+const Login = () => {
+    return(
+        <button className="Login">
+            Sign in with Github
+        </button>
+    )
+}
+
+export default Login
